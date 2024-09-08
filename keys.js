ed0a3a991c6c3a21eb0a5b2054bb262ef1b9308b
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-08 11:55:40 +0600

    object keys  values, nasted , delete

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+  brand: "Apple",
+  price: 100000,
+  processor: "M1",
+  hdd: "512GB",
+  monitor: "hp",
+};
+
+const keys = Object.keys(computer);
+console.log(keys);
---
+++ values.js
@@ -0,0 +1,9 @@
+const computer = {
+  brand: "Apple",
+  price: 100000,
+  processor: "M1",
+  hdd: "512GB",
+  monitor: "hp",
+};
+const values = Object.values(computer);
+console.log(values);
